e9967329f34e0730f927107eb9a658a9001e3ffc
최현준 <qorrhktk66@naver.com>
2025-08-23 17:47:57 +0900

    메이플랜드 창에 포커스하는 기능 추가 (라이브러리 누락)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.github.kwhat:jnativehook:2.2.2")
+    implementation("net.java.dev.jna:jna:5.13.0")
+    implementation("net.java.dev.jna:jna-platform:5.13.0")
 }
 
 tasks.test {
